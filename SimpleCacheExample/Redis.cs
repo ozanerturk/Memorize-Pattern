bc154dda2994ca2a744bf81df5bf659cbcd64cb8
Ozan ERTÜRK <ozanerturk17@gmail.com>
2018-10-03 14:14:15 +0300

    Update Redis.cs

--- SimpleCacheExample/Redis.cs
+++ SimpleCacheExample/Redis.cs
@@ -15,32 +15,18 @@ namespace SimpleCacheExample
             _configurationOptions = new ConfigurationOptions();
             _configurationOptions.EndPoints.Add("localhost");
             _configurationOptions.ConnectRetry = 1;
-
-            int? timeout = 5000;
-            if (timeout != null) //Configte timeout değeri tanımlanmış ise o değer kullanılır. Default: 5000 ms.
-                _configurationOptions.ConnectTimeout = timeout.Value;
-            else
-                _configurationOptions.ConnectTimeout = 500;
+            _configurationOptions.ConnectTimeout = 500;
 
 
         }
 
         public IDatabase GetDb(int storeId)
         {
-            //Try catch kullanım sebebi InternalExfException tipindeki hataların özel olarak loglanması ve monitor edilmesi ihtiyacı
-            //Üstteki public metotlar hata fırlatabilirler, bunu handle etmek burayı çağıran yerin sorumluluğu
-            try
-            {
-                if (_redisConnection == null || !_redisConnection.IsConnected)
-                    _redisConnection = ConnectionMultiplexer.Connect(_configurationOptions);
+            if (_redisConnection == null || !_redisConnection.IsConnected)
+                _redisConnection = ConnectionMultiplexer.Connect(_configurationOptions);
 
-                _db = _redisConnection.GetDatabase(storeId);
-            }
-            catch (Exception)
-            {
-
-            }
-
+            _db = _redisConnection.GetDatabase(storeId);
+            
             return _db;
         }
     }
