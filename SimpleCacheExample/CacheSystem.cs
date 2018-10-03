a09626063f85172c4ba8f6f53eaaace0e0b819cf
Ozan ERTÜRK <ozanerturk17@gmail.com>
2018-10-03 14:12:38 +0300

    redis imp

--- SimpleCacheExample/CacheSystem.cs
+++ SimpleCacheExample/CacheSystem.cs
@@ -1,28 +1,41 @@
-﻿using System;
+﻿using StackExchange.Redis;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Xml.Serialization;
 
 namespace SimpleCacheExample
 {
     public static class CacheSystem
     {
+
         public static Hashtable Cache = new Hashtable();//could be replace with redis :)
 
         public static IEnumerable<TSource> Cacheble<TSource>(this IQueryable<TSource> source)
         {
+            Redis redis = new Redis();
+            var db = redis.GetDb(1);
+           
+            RedisJsonSerializer serialize = new RedisJsonSerializer();
+
             var key = source.ToString();
-            if (Cache.ContainsKey(key))
+
+            var redisValue =db.StringGet(key);
+            if (!string.IsNullOrEmpty(redisValue))
             {
                 Console.WriteLine("Cache Hit");
-                return ((IEnumerable<TSource>)Cache[key]).AsEnumerable();
+                var result = serialize.Deserialize(redisValue);
+                return ((IEnumerable<TSource>)result).AsEnumerable();
             }
             else
             {
                 Console.WriteLine("Cache Miss");
                 Console.WriteLine("Caching...");
                 var result = source.AsEnumerable();
-                Cache.Add(key, result);
+                redisValue = serialize.Serialize(result);
+                db.StringSet( key, redisValue);
                 return result;
             }
         }
---
+++ SimpleCacheExample/Redis.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using StackExchange.Redis;
+using System.Threading;
+
+namespace SimpleCacheExample
+{
+    public class Redis 
+    {
+        private readonly ConfigurationOptions _configurationOptions;
+        private IDatabase _db;
+        private ConnectionMultiplexer _redisConnection;
+
+        public Redis()
+        {
+            _configurationOptions = new ConfigurationOptions();
+            _configurationOptions.EndPoints.Add("localhost");
+            _configurationOptions.ConnectRetry = 1;
+
+            int? timeout = 5000;
+            if (timeout != null) //Configte timeout değeri tanımlanmış ise o değer kullanılır. Default: 5000 ms.
+                _configurationOptions.ConnectTimeout = timeout.Value;
+            else
+                _configurationOptions.ConnectTimeout = 500;
+
+
+        }
+
+        public IDatabase GetDb(int storeId)
+        {
+            //Try catch kullanım sebebi InternalExfException tipindeki hataların özel olarak loglanması ve monitor edilmesi ihtiyacı
+            //Üstteki public metotlar hata fırlatabilirler, bunu handle etmek burayı çağıran yerin sorumluluğu
+            try
+            {
+                if (_redisConnection == null || !_redisConnection.IsConnected)
+                    _redisConnection = ConnectionMultiplexer.Connect(_configurationOptions);
+
+                _db = _redisConnection.GetDatabase(storeId);
+            }
+            catch (Exception)
+            {
+
+            }
+
+            return _db;
+        }
+    }
+}
